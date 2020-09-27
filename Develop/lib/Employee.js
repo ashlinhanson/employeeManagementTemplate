164ff3184ca601d7bd97526094cf4ac031a3e572
ashlinhanson <ashlinhanson@gmail.com>
2020-09-27 18:58:26 -0500

    employee constructor added

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,29 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    };
+
+    getName(){
+        return(this.name);
+    };
+
+    getId(){
+        return(this.id);
+    };
+
+    getEmail(){
+        return(this.email);
+    };
+
+    getPosition(){
+        return(this.position)
+    };
+};
+
+module.exports = Employee;
+
+
