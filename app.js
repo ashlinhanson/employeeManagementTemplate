96ee32a17809bec557e4e55964f9af74e7a5aab1
ashlinhanson <ashlinhanson@gmail.com>
2020-09-28 10:25:53 -0500

    added internfunction and writefile function

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ function engineerInfo(){
             name: "email"
         },
         {
-            message: "What is the engineer's github username?",
+            message: "What is the engineer's Github username?",
             name: "github"
         }
     ]).then(function(answer){
@@ -76,4 +76,42 @@ function engineerInfo(){
 
         employeeInfo();
     })
-}
+};
+
+function internInfo(){
+    return inquirer.prompt([
+        {
+            message: "What is the intern's name?",
+            name: "name"
+        },
+        {
+            message: "What is the intern's id?",
+            name: "id"
+        },
+        {
+            message: "What is the intern's email?",
+            name: "email"
+        },
+        {
+            message: "What school does the intern go to?",
+            name: "school"
+        }
+    ]).then(function(answer){
+        let intern = new Intern(answer.name, answer.id, answer.email, answer.school)
+        team.push(intern);
+
+        employeeInfo();
+    })
+};
+
+function generateHTML(fileName, data) {
+    fs.writeFile(fileName, data, "utf8", function(err){
+        if(err){
+            throw err;
+        }
+        console.log("All done! The employee info has been completed!");
+    });
+};
+
+employeeInfo();
+
