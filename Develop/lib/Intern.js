c85b772fe63309771fcad5c05a24538dd49ff507
ashlinhanson <ashlinhanson@gmail.com>
2020-09-27 19:04:10 -0500

    added intern

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+const Employee = require("../lib/Employee.js");
+
+class Intern extends Employee {
+    constructor(name, id, email, school){
+        super(name, id, email);
+        this.school;
+    };
+
+    getSchool(){
+        return(this.school);
+    };
+
+    getPosition(){
+        return "Intern";
+    };
+};
+
+module.exports = Intern;
